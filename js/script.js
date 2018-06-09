995772bbd7552981493cc88e2adc59acfb3bf714
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-06-09 20:24:37 +0200

    add reset button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+// external js: flickity.pkgd.js
+
+var flkty = new Flickity('.carousel');
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
 });
