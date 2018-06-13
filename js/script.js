afb5c98a9d706bc42ecf9f4b11db4819e5969937
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-06-13 18:18:44 +0200

    change js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,64 @@
+var coords = [{
+        name: 'Poznan',
+        kordy: '{lat: 52.406374, lng: 16.925168}'
 
-var kordy = [
-{
-	Name:'Poznan',
-	Kordy:'{lat: 52.406374, lng: 16.925168}'
-	
-},
-{
-	Name:'uluru',
-	Kordy:'{lat: -25.344, lng: 131.036}'
-}
+    },
+    {
+        name: 'uluru',
+        kordy: '{lat: -25.344, lng: 131.036}'
+    }
 ]
-var uluru = {lat: -25.344, lng: 131.036};
+var uluru = {
+    	lat: -25.344,
+   		lng: 131.036
+	};
 
 window.initMap = function() {
 
-  var map = new google.maps.Map(
-  document.getElementById('map'), {zoom: 4, center: uluru});
-  var marker = new google.maps.Marker({position: kordy[1].Kordy, map: map});
-  
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+    var marker = new google.maps.Marker({
+        position: coords[1].Kordy,
+        map: map
+    });
+
 };
 
-(function(){ 
-	
-	var templateItem = document.getElementById('template').innerHTML;
-	Mustache.parse(templateItem);
-	var listItems = '';
-		for(var i = 0; i < Data.length; i++){
-		console.log(Data);
-		listItems += Mustache.render(templateItem, Data[i]);
-	}
-		var results = document.getElementById('results');
-	
-		results.insertAdjacentHTML('beforeend', listItems);
-	});
+(function() {
+
+    var templateItem = document.getElementById('template').innerHTML;
+    Mustache.parse(templateItem);
+    var listItems = '';
+    for (var i = 0; i < data.length; i++) {
+        listItems += Mustache.render(templateItem, data[i]);
+    }
+    var results = document.getElementById('results');
+
+    results.insertAdjacentHTML('beforeend', listItems);
+});
 
 var flkty = new Flickity('.carousel');
 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
+buttons = fizzyUIUtils.makeArray(buttons);
 
-buttonGroup.addEventListener( 'click', function( event ) {
+buttonGroup.addEventListener('click', function(event) {
 
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
 
 });
 
 var progressBar = document.querySelector('.progress-bar')
 
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+flkty.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
